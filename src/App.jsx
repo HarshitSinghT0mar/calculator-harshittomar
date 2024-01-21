71e84d811272fa2c56b3acd42ddedcd2997581c2
Harshit Singh Tomar <harshit.nitmz@gmail.com>
2024-01-21 17:20:16 +0530

    style: basic ui for calculator

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { calculatorButtons } from "./calculatorBtns";
 
 function App() {
-
+  const [input, setInput] = useState();
+  const [result, setResult] = useState(0);
 
   return (
-    <div className='app-container'>
-    
+    <div className="app-container">
+      <div className="output-container">
+        <div className="input">{input}</div>
+        <div className="result">{result}</div>
+      </div>
+
+      <div className="btn-container">
+        {calculatorButtons.map((btn, index) => {
+          return (
+            <button value={btn} className="btn" key={index}>
+              {btn}
+            </button>
+          );
+        })}
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
